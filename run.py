5fb1a1317bf7857155a4097d09cd46fda64bd3d4
Joby-r <joby.reeves11@gmail.com>
2025-02-15 14:12:35 +0000

    Made changes to main .py file, installed more dependencies

--- run.py
+++ run.py
@@ -7,13 +7,33 @@ import plotly.express as px
 import plotly
 import plotly.graph_objects as go   
 
+# CHECKING YFINANCE
 
 # apple = yf.Ticker("AAPL")
-
 # hist = apple.history(period="1mo")
-
 # print(hist)
 
-df = px.data.iris()
-fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
-fig.show()
+# CHECKING PLOTLY
+
+# df = px.data.iris()
+# fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
+# fig.show()
+
+# CHECKING SEABORN  
+
+# stocks_df = pd.read_csv('stock-prices.csv')
+# print(stocks_df.head())
+
+stocks_df = pd.read_csv('stock-prices.csv')
+fig = px.line(title= "AMZN scatter plot")
+fig.add_scatter(x = stocks_df["Date"], y = stocks_df["AMZN"],name = "ADJ close")
+fig.show() 
+
+def plot_financial_data(df, title):
+  fig=px.line(title=title)
+  for i in df.columns[1:]:
+    fig.add_scatter(x=df["Date"], y = df[i], name = i)
+    fig.update_traces(line_width=5)
+    fig.update_layout({"plot_bgcolor":"white"})
+  fig.show()
+plot_financial_data(stocks_df, "Test")
